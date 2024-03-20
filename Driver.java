53733ef151821ec6c769c97c143216660c83ba86
Gavin Chock-Chiong <gavin.chockchiong@gmail.com>
2024-03-19 22:15:50 -0400

    changing back to partTwo branch

--- Driver.java
+++ Driver.java
@@ -4,6 +4,14 @@ public class Driver {
         String[] manifest_part1 = Movie.readManifest("manifests_txt_files\\part1_manifest.txt");
 
         Movie.do_part_one_manifest(manifest_part1);
+
+        String[] manifest_part2 = Movie.readManifest("manifests_txt_files\\part2_manifest.txt");
+
+        
+        
+
+ 
+
     }
 
 }
